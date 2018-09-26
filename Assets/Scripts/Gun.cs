d5727b1ada76e90cba607144bd48b3d4ba5acdea
Jeffrey McLemore <jeffreymclemore@jeffreys-mbp.lan>
2018-09-26 01:16:21 -0400

    added gun scripts

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+    // The speed at which the gun will fire
+    public float fireRate;
+
+    // The last time the gun was fired
+    protected float lastFireTime;
+
+	// Use this for initialization
+	void Start () {
+        // Sets last fire time to 15 seconds ago
+        // So player can shoot immediately upon playing
+        lastFireTime = Time.time - 15;
+	}
+	
+	// Update is called once per frame
+    protected virtual void Update()
+    {
+    }
+
+    protected void Fire()
+    {
+    }
+
+}
