190aafbd6ba1c91da2f21c7a77cf03ffea23cf07
Jeffrey McLemore <jeffreymclemore@jeffreys-mbp.lan>
2018-10-04 19:50:47 -0400

    added alien move,fire anim

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class Alien : MonoBehaviour {
 
-    public int health; // HP of the alien
-    public int range; // How far the alien must be before shooting its gun
-    public float fireRate; // How fast the alien can fire its gun
-    public Transform missileFireSpot;  // Location from which the special ability is launched
-    UnityEngine.AI.NavMeshAgent agent; // Ref to the NavMesh Agent component
-    private Transform player; // Ref to the player which the alien tries to find
-    private float timeLastFired;
-    private bool isDead; // If the alien is alive or not
+    public int health; // HP of the alien ******
+    public int firingRange; // How far the alien must be before shooting its gun
+    public float firingRate; // How fast the alien can fire its gun
+    public Transform firingLocation;  // Coordinate on model where bullets fire
+    UnityEngine.AI.NavMeshAgent agent; // Ref to the NavMesh Agent component ******
+    private Transform player; // Ref to the player which the alien tries to find ******
+    private float timeLastFired; // ****
+    private bool isDead; // If the alien is alive or not ****
+    public Animator alien; // For playing animations
 
 	// Use this for initialization
 	void Start () {
@@ -31,16 +32,17 @@ public class Alien : MonoBehaviour {
         agent.SetDestination(player.position); // Alien uses Nav Mesh to find player
 
         float distanceToPlayer = Vector3.Distance(transform.position, player.position);
-        bool alienIsWithinRange = distanceToPlayer < range;
-        float timeSinceLastShot = Time.time - timeLastFired;
-        bool alienCanFireAgain = timeSinceLastShot > fireRate;
+        bool alienIsWithinFiringRange = distanceToPlayer < firingRange;
+        float timeSinceLastFired = Time.time - timeLastFired;
+        bool alienCanFireAgain = timeSinceLastFired > firingRate;
 
-        if (alienIsWithinRange && alienCanFireAgain) {
+        if (alienIsWithinFiringRange && alienCanFireAgain) {
             timeLastFired = Time.time;
             fire();
         }
     }
+
     private void fire() {
-        Debug.Log("Firing...");
+        alien.Play("fire");
     }
 }
